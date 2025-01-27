32f7142eac745ae3999e0741d28e0089db0ecc77
Malte Rozenbeek <malte.rozenbeek@gmail.com>
2025-01-27 17:43:17 +0100

    hero/intro layout for mobile done, linking, animations and desktop left

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import NavbarMobile from "./components/NavbarMobile";
+import Hero from "./components/Hero";
 
 const App = () => {
   return (
     <>
       <NavbarMobile />
+      <Hero />
     </>
   );
 };
---
+++ src/components/Hero.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./scss/_hero.scss";
+import linkedInIcon from "../assets/devicon_linkedin.svg";
+import githubIcon from "../assets/akar-icons_github-fill.svg";
+import gmailIcon from "../assets/logos_google-gmail.svg";
+
+const Hero = () => {
+  return (
+    <div className="hero-container">
+      <div className="upper">
+        <div className="img"></div>
+      </div>
+      <div className="lower">
+        <div className="hero-text-container">
+          <p className="title">Turning Ideas into Impact</p>
+          <p className="text">
+            I’m a developer with a passion for crafting intuitive web and app
+            experiences. From concept to deployment, I focus on delivering
+            clean, scalable solutions that make a meaningful impact.
+          </p>
+        </div>
+        <div className="contact-icons">
+          <img src={linkedInIcon} alt="linkedin icon" className="linkedin" />
+          <img src={githubIcon} alt="github icon" className="github" />
+          <img src={gmailIcon} alt="gmail icon" className="gmail" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
