4cab736b34e159d9f38f9b3bcb0d8e19a5452379
Malte Rozenbeek <malte.rozenbeek@gmail.com>
2025-01-27 12:31:22 +0100

    navigation bar added for mobile, expandable hamburger menu done. Animation on icon and another background color for the expandable menu left

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import NavbarMobile from "./components/NavbarMobile";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <NavbarMobile />
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/NavbarMobile.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { IoReorderTwo } from "react-icons/io5";
+import { IoClose } from "react-icons/io5";
+import "./scss/_navbarMobile.scss";
+
+const NavbarMobile = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <>
+      <header>
+        <nav>
+          <div className="nav-text-container">
+            <p className="name">Malte Rozenbeek</p>
+            <p className="job-title">Fullstack webb & app developer</p>
+          </div>
+          <IoReorderTwo size={24} onClick={toggleNavbar} />
+        </nav>
+      </header>
+      <div className={`expanded-nav ${isOpen ? "open" : ""}`}>
+        <div className="close-container">
+          <IoClose size={24} onClick={toggleNavbar} />
+        </div>
+        <div className="nav-options">
+          <li>Projects</li>
+          <li>FAQ</li>
+          <li>Contact</li>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default NavbarMobile;
