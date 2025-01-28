7fb55c7c9448ee6431ee9f668f3829eb7625bb87
Malte Rozenbeek <malte.rozenbeek@gmail.com>
2025-01-28 09:35:52 +0100

    tooltip added to all the icons to explain to the users what they are looking at

--- src/components/MyStack.tsx
+++ src/components/MyStack.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./scss/_myStack.scss";
+import { Tooltip } from "@mui/material";
+
 import SectionHeader from "./SectionHeader";
 import htmlIcon from "../assets/html.svg";
 import typeScriptIcon from "../assets/Vector.svg";
@@ -19,29 +21,56 @@ const MyStack = () => {
     <>
       <SectionHeader
         title="My stack and frameworks"
-        subheader="See what technology i use"
+        subheader="See what technology I use"
       />
       <div className="my-stack-content">
+        {/* Frontend / Design Stack */}
         <div className="stack">
-          <p>Frontend / Design </p>
+          <p>Frontend / Design</p>
           <div className="icons">
-            <img src={htmlIcon} alt="html icon" />
-            <img src={typeScriptIcon} alt="html icon" />
-            <img src={reactIcon} alt="react icon" />
-            <img src={reactIcon} alt="react icon" />
-            <img src={sassIcon} alt="sass icon" />
-            <img src={tailWindIcon} alt="tailwind icon" />
-            <img src={bootSrapIcon} alt="bootstrap icon" />
-            <img src={figmaIcon} alt="figma icon" />
+            <Tooltip title="HTML5">
+              <img src={htmlIcon} alt="HTML icon" />
+            </Tooltip>
+            <Tooltip title="TypeScript">
+              <img src={typeScriptIcon} alt="TypeScript icon" />
+            </Tooltip>
+            <Tooltip title="React">
+              <img src={reactIcon} alt="React icon" />
+            </Tooltip>
+            <Tooltip title="React Native">
+              <img src={reactIcon} alt="React Native icon" />
+            </Tooltip>
+            <Tooltip title="SASS">
+              <img src={sassIcon} alt="SASS icon" />
+            </Tooltip>
+            <Tooltip title="Tailwind CSS">
+              <img src={tailWindIcon} alt="Tailwind CSS icon" />
+            </Tooltip>
+            <Tooltip title="Bootstrap">
+              <img src={bootSrapIcon} alt="Bootstrap icon" />
+            </Tooltip>
+            <Tooltip title="Figma">
+              <img src={figmaIcon} alt="Figma icon" />
+            </Tooltip>
           </div>
         </div>
+
+        {/* Backend / Database Stack */}
         <div className="stack">
           <p>Backend / DB</p>
           <div className="icons">
-            <img src={mongoDbIcon} alt="mongoDb icon" />
-            <img src={nodeJSIcon} alt="nodeJs icon" />
-            <img src={expressJsICon} alt="expressJs icon" />
-            <img src={mySqlIcon} alt="mySgl icon" />
+            <Tooltip title="MongoDB">
+              <img src={mongoDbIcon} alt="MongoDB icon" />
+            </Tooltip>
+            <Tooltip title="Node.js">
+              <img src={nodeJSIcon} alt="Node.js icon" />
+            </Tooltip>
+            <Tooltip title="Express.js">
+              <img src={expressJsICon} alt="Express.js icon" />
+            </Tooltip>
+            <Tooltip title="MySQL">
+              <img src={mySqlIcon} alt="MySQL icon" />
+            </Tooltip>
           </div>
         </div>
       </div>
